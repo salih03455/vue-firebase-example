c2c87b2e1a8f48a5aa38cc99849553df90c2c858
salih03455 <salih03455@gmail.com>
2019-06-02 17:46:16 +0300

    home carousel yapıldı

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>
-    <p>The home page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large to="/meetups" class="primary">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large to="/meetup/new" class="primary">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :key="meetup.id"
+            :src="meetup.imageUrl"
+          >
+            <div class="title">
+              {{meetup.title}}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      meetups: [
+        { imageUrl: 'https://img-s1.onedio.com/id-54622b6e32abfb2b73ea9d74/rev-0/w-635/listing/f-jpg-webp/s-f471cc6281dc7b71142ac415699de56b93e5361d.webp', id: 'asdasdds2323', title: 'Meetup in İstanbul' },
+        { imageUrl: 'https://iasbh.tmgrup.com.tr/8b9609/752/395/212/0/1945/910?u=https://isbh.tmgrup.com.tr/sbh/2018/11/09/dunya-liderleri-pazar-gunu-pariste-toplanacak-1541769911649.jpg', id: 'dfgfgdfgrf4545', title: 'Meetup in Paris' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgb(0, 0, 0, 0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
